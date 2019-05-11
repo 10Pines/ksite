e7440a0e7cf42092f289b945f068424bf010f9ff
Erwin Debusschere <erwincdl@gmail.com>
2019-05-11 02:00:06 -0300

    Fixes fat jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.31"
+    id("com.github.johnrengelman.shadow") version "5.0.0"
 }
 
 group = "com.10pines.kotlingreenhouse"
@@ -24,10 +26,15 @@ dependencies {
     implementation("com.typesafe.akka", "akka-actor-typed_2.12", "2.5.22")
     implementation("com.typesafe.akka", "akka-slf4j_2.12", "2.5.22")
     implementation("ch.qos.logback", "logback-classic", "1.2.3")
-
-    //implementation("org.slf4j", "slf4j-simple", "1.7.26")
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<ShadowJar> {
+    manifest {
+        attributes["Main-Class"] = "ServerKt"
+    }
+    mergeServiceFiles()
 }
